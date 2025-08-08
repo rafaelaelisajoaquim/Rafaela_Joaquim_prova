1eba31ac73e400850381a377a7ba46d2d517be12
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-08-08 17:12:06 -0300

    Página de cada usuário
    Quando o usuário fazer login irá aparecer a sua tela.

---
+++ principal.php
@@ -0,0 +1,45 @@
+<?php   
+    session_start();
+    require_once'conexao.php';
+
+    if(!isset($_SESSION['usuario'])){
+        header("Location: login.php");
+        exit();
+    }
+
+    //OBTENDO O NOME DO PESFIL DO USUARIO LOGADO
+    $id_perfil = $_SESSION['perfil'];
+    $sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+    $stmtPerfil=$pdo->prepare($sqlPerfil);
+    $stmtPerfil->bindParam(':id_perfil',$sqlPerfil);
+    $stmtPerfil->execute();
+    $perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+    $nome_perfil = $perfil['nome_perfil'];
+
+    //DEFINICAO DAS PERMISSOES POR perfil
+    $permissoes= [
+        1 => ["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php",
+        "cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php",
+        "cadastro_funcionario.php"]],
+
+        "Buscar"=>["buscar_usuario.php","buscar_perfil.php",
+        "buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php",
+        "buscar_funcionario.php"],
+
+        "Alterar"=>["alterar_usuario.php","alterar_perfil.php",
+        "alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php",
+        "alterar_funcionario.php"],
+
+        "Excluir"=>["excluir_usuario.php","excluir_perfil.php",
+        "excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php",
+        "excluir_funcionario.php"]];
+
+            2 => ["Cadastrar"=>["cadastro_cliente.php"]],
+    
+            "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+    
+            "Alterar"=>["alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php"],
+
+            "Excluir"=>["excluir_produto.php"];
+    ]
+?>
