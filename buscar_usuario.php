f57a3d8309b550829219732d1a17a8ec8a362826
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-08-11 17:03:30 -0300

    Buscar Usuário
    Faz a pesquisa no banco para buscar os usuários.

---
+++ buscar_usuario.php
@@ -0,0 +1,44 @@
+<?php
+    session_start();
+    require_once 'conexao.php';
+
+    //VERIFICA SE O USUÁRIO TEM PERMISSÃO DE amd OU secretaria
+    if($_SESSION['perfil'] !=1 && $_SESSION['perfil']!=2){
+        echo "<script>alert('Acesso negado!');window.location.href='principal.php';</script>";
+        exit();
+    }
+
+    $usuario = []; //INICIALIZA A VARIAVEL PARA EVITAR ERROS
+    //SE O FORMULÁRIO FOR ENVIADO, BUSCA OPELO id OU nome
+    if($_SERVER["REQUEST_METHOD"]=="POST" && !empty($_POST['busca'])){
+        $busca = trim($_POST['busca']);
+
+        //VERIFICA SE A BUSCA É UM NUMERO OU UM NOME
+        if(is_numeric($busca)){
+            $sql = "SELECT * FROM usuario WHERE id_usuario = :busca ORDER BY nome ASC";
+            $stmt=$pdo->prepare($sql);
+            $stmt->bindParam(':busca', $busca, PDO::PARAM_INT);
+        } else {
+                $sql = "SELECT * FROM usuario WHERE nome LIKE :busca_nome ORDER BY nome ASC";
+                $stmt=$pdo->prepare($sql);
+                $stmt->bindValue(':busca_nome', "%$busca%", PDO::PARAM_STR);
+        }
+    } else{
+        $sql = "SELECT * FROM usuario ORDER BY nome ASC";
+        $stmt=$pdo->prepare($sql);
+    }
+    $stmt->execute();
+    $usuario = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Usuário</title>
+</head>
+<body>
+    
+</body>
+</html>
