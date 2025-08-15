9d873e4e9afb925a05c3a824bd434f8463b05676
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-08-15 17:06:53 -0300

    Alterar usuário
    Altera o usuário,.

--- alterar_usuario.php
+++ alterar_usuario.php
@@ -3,7 +3,7 @@
     require_once 'conexao.php';
 
     //VERIFICA SE O USUÁRIO TEM PERMISSÃO DE adm
-    if($_SESSION['perfil'] !=1 && $_SESSION['perfil']!=2){
+    if($_SESSION['perfil'] !=1){
         echo "<script>alert('Acesso negado!');window.location.href='principal.php';</script>";
         exit();
     }
@@ -22,8 +22,9 @@
             } else {
                 $sql = "SELECT * FROM usuario WHERE nome LIKE :busca_nome";
                 $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(':busca_nome', "$busca%", PDO::PARAM_STR);
+                $stmt->bindValue(':busca_nome', "$busca%", PDO::PARAM_STR);
             }
+            
             $stmt->execute();
             $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -51,7 +52,6 @@
     <form action="alterar_usuario.php" method="POST">
         <label for="busca_usuario">Digite o ID ou NOME do usuário</label>
         <input type="text" id="busca_usuario" name="busca_usuario" required onkeyup="buscarSugestoes()">
-        <button type="submit">Pesquisar</button>
 
         <!-- div PARA SUGESTÕES DE USUÁRIOS -->
         <div id="sugestoes"></div>
@@ -62,7 +62,7 @@
         <!-- FORM PARA ALTERAR USUARIO -->
          <form action="processa_alteracao_usuario.php" method="POST">
             <input type="hidden" name="id_usuario" value="<?=htmlspecialchars($usuario['id_usuario'])?>">
-        </form>
+
 
         <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome" value="<?=htmlspecialchars($usuario['nome'])?>" required>
@@ -72,9 +72,23 @@
 
         <label for="id_perfil">Perfil:</label>
         <select id="id_perfil" name="id_perfil">
-            <option value="1"<?=$usuario['id_perfil']== 1 ?'select':''?>Administrador</option>
-            <option value="1"<?=$usuario['id_perfil']== 1 ?'select':''?>Administrador</option>
+            <option value="1"<?=$usuario['id_perfil']== 1 ?'select':''?>>Administrador</option>
+            <option value="2"<?=$usuario['id_perfil']== 2 ?'select':''?>>Secretario</option>
+            <option value="3"<?=$usuario['id_perfil']== 3 ?'select':''?>>Almoxarife</option>
+            <option value="4"<?=$usuario['id_perfil']== 4 ?'select':''?>>Cliente</option>
+        </select>
 
+    <!-- SE O USUARIO FOR ADM, EXIBIR OPÇAO DE ALTERAR SENHA -->
+     <?php if($_SESSION['perfil'] == 1 ): ?>
+            <label for='nova_senha'>Nova Senha</label>
+            <input type="password" id="nova_senha" name="nova_senha">
+        <?php endif; ?>
+
+        <button type="submit">Alterar</button>
+        <button type="reset">Cancelar</button>
+    </form>
+        <?php endif; ?>
+        <a href="principal.php">Voltar</a>
 
 
     
