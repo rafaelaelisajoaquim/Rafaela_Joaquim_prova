8f2e1c8aea5ff875697a8e94ead01fd3d0b2f007
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-08-17 13:23:40 -0300

    Página Principal
    Página que aparece após realizar o login.

--- principal.php
+++ principal.php
@@ -18,35 +18,24 @@
 
     //DEFINICAO DAS PERMISSOES POR perfil
     $permissoes= [
-        1 => ["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php",
-        "cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php",
-        "cadastro_funcionario.php"],
-
-        "Buscar"=>["buscar_usuario.php","buscar_perfil.php",
-        "buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php",
-        "buscar_funcionario.php"],
-
-        "Alterar"=>["alterar_usuario.php","alterar_perfil.php",
-        "alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php",
-        "alterar_funcionario.php"],
-
-        "Excluir"=>["excluir_usuario.php","excluir_perfil.php",
-        "excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php",
-        "excluir_funcionario.php"]],
-
-            2 => ["Cadastrar"=>["cadastro_cliente.php"],
-            "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
-            "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
-            "Excluir"=>["excluir_produto.php"]],
-
-            3 => ["Cadastrar"=>["cadastro_fornecedor.php","cadastro_produto.php"],
-            "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
-            "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
-            "Excluir"=>["excluir_produto.php"]],
-
-            4 => ["Cadastrar"=>["cadastro_cliente.php"],
-            "Buscar"=>["buscar_produto.php"],
-            "Alterar"=>["alterar_cliente.php"]],      
+        1 =>["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"],
+        "Buscar"=>["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
+        "Alterar"=>["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
+        "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]],
+    
+        2 =>["Cadastrar"=>["cadastro_cliente.php"],
+        "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Alterar"=>["alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php"],
+        "Excluir"=>["excluir_produto.php"]],
+    
+        3 =>["Cadastrar"=>["cadastro_fornecedor.php","cadastro_produto.php"],
+        "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
+        "Excluir"=>["excluir_produto.php"]],
+    
+        4 =>["Cadastrar"=>["cadastro_cliente.php"],
+        "Buscar"=>["buscar_produto.php"],
+        "Alterar"=>["alterar_cliente.php"]],      
     ];
 
     //OBTENDO AS OPÇÕES DISPONIVEIS PARA O PERFIL LOGADO
