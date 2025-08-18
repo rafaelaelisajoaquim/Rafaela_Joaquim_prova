996685821befa9e6573402e9130caf1ca975d1ac
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-08-18 17:07:55 -0300

    Alterar Usuário
    Adicionado dropdown.

--- alterar_usuario.php
+++ alterar_usuario.php
@@ -35,6 +35,40 @@
         }
     }
 
+           //OBTENDO O NOME DO PESFIL DO USUARIO LOGADO
+           $id_perfil = $_SESSION['perfil'];
+           $sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+           $stmtPerfil=$pdo->prepare($sqlPerfil);
+           $stmtPerfil->bindParam(':id_perfil',$id_perfil);
+           $stmtPerfil->execute();
+           $perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+           $nome_perfil = $perfil['nome_perfil'];
+       
+           //DEFINICAO DAS PERMISSOES POR perfil
+           $permissoes= [
+               1 =>["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"],
+               "Buscar"=>["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
+               "Alterar"=>["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
+               "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]],
+           
+               2 =>["Cadastrar"=>["cadastro_cliente.php"],
+               "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+               "Alterar"=>["alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php"],
+               "Excluir"=>["excluir_produto.php"]],
+           
+               3 =>["Cadastrar"=>["cadastro_fornecedor.php","cadastro_produto.php"],
+               "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+               "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
+               "Excluir"=>["excluir_produto.php"]],
+           
+               4 =>["Cadastrar"=>["cadastro_cliente.php"],
+               "Buscar"=>["buscar_produto.php"],
+               "Alterar"=>["alterar_cliente.php"]],      
+           ];
+       
+           //OBTENDO AS OPÇÕES DISPONIVEIS PARA O PERFIL LOGADO
+           $opcoes_menu = $permissoes["$id_perfil"];
+
 ?>
 
 <!DOCTYPE html>
@@ -90,6 +124,23 @@
     </style>
 </head>
     <body>
+        <nav>
+            <ul class="menu">
+                    <?php foreach($opcoes_menu as $categoria => $arquivos): ?>
+                        <li class="dropdown">
+                            <a href="#"><?=$categoria ?></a>
+                            <ul class="dropdown-menu">
+                                <?php foreach($arquivos as $arquivo): ?>
+                                    <li>
+                                        <a href="<?=$arquivo ?>">
+                                        <?=ucfirst(str_replace("_"," ",basename($arquivo,".php"))); ?></a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>    
+        </nav>
         <h2>Alterar Usuário</h2>
         <form action="alterar_usuario.php" method="POST">
             <label for="busca_usuario">Digite o ID ou NOME do usuário</label>
