a014d03ccbd2e8db190baccbb36e92b88f93e890
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-08-18 17:08:49 -0300

    Excluir Usuário
    Adicionado dropdown.

--- excluir_usuario.php
+++ excluir_usuario.php
@@ -27,11 +27,45 @@
         $stmt->bindParam(':id', $id_usuario);
        
         if($stmt->execute()){
-            echo "<script>alert('Usuário excluido com sucesso!');window.location.href='excluir_usuario.php';</script>";
+            echo "<script>alert('Usuário excluído com sucesso!');window.location.href='excluir_usuario.php';</script>";
         } else {
         echo "<script>alert('Erro ao excluir usuário!');</script>";
         }
     }
+
+        //OBTENDO O NOME DO PESFIL DO USUARIO LOGADO
+        $id_perfil = $_SESSION['perfil'];
+        $sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+        $stmtPerfil=$pdo->prepare($sqlPerfil);
+        $stmtPerfil->bindParam(':id_perfil',$id_perfil);
+        $stmtPerfil->execute();
+        $perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+        $nome_perfil = $perfil['nome_perfil'];
+    
+        //DEFINICAO DAS PERMISSOES POR perfil
+        $permissoes= [
+            1 =>["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"],
+            "Buscar"=>["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
+            "Alterar"=>["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
+            "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]],
+        
+            2 =>["Cadastrar"=>["cadastro_cliente.php"],
+            "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+            "Alterar"=>["alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php"],
+            "Excluir"=>["excluir_produto.php"]],
+        
+            3 =>["Cadastrar"=>["cadastro_fornecedor.php","cadastro_produto.php"],
+            "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+            "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
+            "Excluir"=>["excluir_produto.php"]],
+        
+            4 =>["Cadastrar"=>["cadastro_cliente.php"],
+            "Buscar"=>["buscar_produto.php"],
+            "Alterar"=>["alterar_cliente.php"]],      
+        ];
+    
+        //OBTENDO AS OPÇÕES DISPONIVEIS PARA O PERFIL LOGADO
+        $opcoes_menu = $permissoes["$id_perfil"];
 ?>
 
 <!DOCTYPE html>
@@ -41,6 +75,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Excluir Usuário</title>
     <link rel="stylesheet" href="styles.css">
+    
     <style>
             body {
             font-family: Arial, sans-serif;
@@ -101,24 +136,38 @@
         
         .footer {
             background-color: #333;
-            padding: 20px;
+            margin-top: 10px;
+            padding: 10px;
             width: 100%;
             box-sizing: border-box;
-            margin-bottom: 0;
             position: absolute;
-            bottom: 0;
-            left: 0;
         }
 
         .footer p {
             text-align: center;
             color: white;
-            margin: 0;
             font-size: 14px;
         } 
     </style>
 </head>
     <body>
+<nav>
+    <ul class="menu">
+            <?php foreach($opcoes_menu as $categoria => $arquivos): ?>
+                <li class="dropdown">
+                    <a href="#"><?=$categoria ?></a>
+                    <ul class="dropdown-menu">
+                        <?php foreach($arquivos as $arquivo): ?>
+                            <li>
+                                <a href="<?=$arquivo ?>">
+                                <?=ucfirst(str_replace("_"," ",basename($arquivo,".php"))); ?></a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>    
+    </nav>
         <h2>Excluir Usuário</h2>
             <?php if(!empty($usuarios)): ?>
                 <table> 
@@ -145,9 +194,8 @@
         <p>Nenhum usuário encontrado</p>
     <?php endif; ?>
 <a class="voltar" href="principal.php">Voltar</a>
-        
-        <footer class="footer">
-            <p>Rafaela Elisa Joaquim | Desenvolvimento de Sistemas</p>
-        </footer>
     </body>
+<footer class="footer">
+    <p>Rafaela Elisa Joaquim | Desenvolvimento de Sistemas</p>
+</footer>
 </html>
