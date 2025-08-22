67e32cd0d04dee01f2a18fda3f626d79313e23d4
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-08-22 16:30:52 -0300

    Buscar Usuário
    Alteração da estilização do rodapé e nas permissões dos usuários.

--- buscar_usuario.php
+++ buscar_usuario.php
@@ -41,25 +41,25 @@
    
        //DEFINICAO DAS PERMISSOES POR perfil
        $permissoes= [
-           1 =>["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"],
-           "Buscar"=>["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
-           "Alterar"=>["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
-           "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]],
-       
-           2 =>["Cadastrar"=>["cadastro_cliente.php"],
-           "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
-           "Alterar"=>["alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php"],
-           "Excluir"=>["excluir_produto.php"]],
-       
-           3 =>["Cadastrar"=>["cadastro_fornecedor.php","cadastro_produto.php"],
-           "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
-           "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
-           "Excluir"=>["excluir_produto.php"]],
-       
-           4 =>["Cadastrar"=>["cadastro_cliente.php"],
-           "Buscar"=>["buscar_produto.php"],
-           "Alterar"=>["alterar_cliente.php"]],      
-       ];
+        1 =>["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"],
+        "Buscar"=>["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
+        "Alterar"=>["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
+        "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]],
+    
+        2 =>["Cadastrar"=>["cadastro_cliente.php"],
+        "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Alterar"=>["alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php"],
+        "Excluir"=>["excluir_produto.php"]],
+    
+        3 =>["Cadastrar"=>["cadastro_fornecedor.php","cadastro_produto.php"],
+        "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
+        "Excluir"=>["excluir_produto.php"]],
+    
+        4 =>["Cadastrar"=>["cadastro_cliente.php"],
+        "Buscar"=>["buscar_produto.php"],
+        "Alterar"=>["alterar_cliente.php"]],   
+    ];
    
        //OBTENDO AS OPÇÕES DISPONIVEIS PARA O PERFIL LOGADO
        $opcoes_menu = $permissoes["$id_perfil"];
@@ -119,10 +119,12 @@
 
         .footer {
             background-color: #333;
-            color:white;
-            padding: 15px;
-            margin-top: 20px;
-            font-size:14px;
+            font-size: 14px;
+            color: white;
+            padding: 10px;
+            margin-top: 30px;
+            margin-left: -8px;
+            margin-right: -8px;
         }
     </style>
 </head>
